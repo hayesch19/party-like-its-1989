d54285e7516c560217e69dd9fca5a8194078e837
hayesch19 <chrishayespro@gmail.com>
2019-08-22 09:02:30 -0400

    V4 8 Bit Gif Added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import Movie from './components/Movie.jsx'
+import PowBox from './images/powbox.gif'
+import Unicorn from './images/unicorn.gif'
 
 class App extends Component {
   render() {
     return (
       <main>
         <header className="header-section">
+          <img className="powbox" src={PowBox} />
           <h1>PARTY LIKE IT'S 1989!!</h1>
+          <img className="powbox" src={PowBox} />
         </header>
         <Movie />
+        <footer>
+          <img className="unicorn" src={Unicorn} />
+        </footer>
       </main>
     )
   }
